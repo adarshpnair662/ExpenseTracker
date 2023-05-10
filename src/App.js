128a1537e9dfed704390bfb337e12ab18522c1ff
AdarshPNair <adarshpnair662@gmail.com>
2023-05-11 00:06:41 +0530

    added Card background to items + date  & made Expense.js seprate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
-  const expense =  [
+  const expenses = [
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -27,12 +25,9 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started</h2>
-      <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date}></ExpenseItem>
-      <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date}></ExpenseItem>
-      <ExpenseItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date}></ExpenseItem>
-      <ExpenseItem title={expense[3].title} amount={expense[3].amount} date={expense[3].date}></ExpenseItem>
-      </div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
+    </div>
   );
 }
 
--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,4 +1,5 @@
-import './ExpenseDate.css'
+import Card from './Card';
+import './ExpenseDate.css';
 
 function ExpenseDate(props)
 {
@@ -6,11 +7,11 @@ function ExpenseDate(props)
     const day = props.date.toLocaleString('en-US', {day: '2-digit'});
     const year = props.date.getFullYear();
     return(
-    <div className="expense-date">
+    <Card className="expense-date">
         <div className="expense-date__month">{month}</div>
         <div className="expense-date__year">{year}</div>
         <div className="expense-date__day">{day}</div>
-        </div>
+        </Card>
         );
 }
 
