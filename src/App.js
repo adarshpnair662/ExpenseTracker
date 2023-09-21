74992b8d312a36507a20860a1904a93f9afef7bb
AdarshPNair <adarshpnair662@gmail.com>
2023-09-21 16:51:49 +0530

    bug fix: Expenseform amount

--- src/App.js
+++ src/App.js
@@ -21,6 +21,12 @@ const DUMMY_EXPENSES = [
     amount: 450,
     date: new Date('2022-01-25'),
   },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date('2021-01-25'),
+  },
 ];
 
 
